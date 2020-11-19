7464090de2e63efc74dbd46eb3b1925efb309588
Michael Miller <icy.arctic.fox@gmail.com>
2020-11-19 09:14:33 -0700

    Artificial workload (sleep)

--- .buildkite/dynamic_script_generator.sh
+++ .buildkite/dynamic_script_generator.sh
@@ -5,7 +5,7 @@ readonly count=$(buildkite-agent meta-data get parallel-count)
 
 echo "steps:"
 echo '  - label: ":ruby: :docker:"'
-echo $'    command: "ruby -e \'puts 42\'"'
+echo $'    command: "ruby -e \'sleep 60; puts 42\'"'
 echo "    parallelism: ${count}"
 echo '    agents:'
 echo '      queue: "${BUILDKITE_AGENT_META_DATA_QUEUE:-default}"'
